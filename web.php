5609a7954d2a6b19934dbb45784b853867832ccf
MauraRBI <maura@rb10.com.br>
2012-10-24 18:14:26 -0200

    Update web.php

--- web.php
+++ web.php
@@ -18,4 +18,5 @@ ChecaLogin();
 		extract($rsF);
 		header('location: webmail/web/index.php?email='.$WEB_EMAIL.'&login='.$WEB_NOME.'&password='.$WEB_SENHA.'&inc_server='.$WEB_POP.'&inc_port='.$WEB_PORT_POP.'&out_server='.$WEB_SMTP.'&out_port='.$WEB_PORT_SMTP.'&EMAILSYS=1'); 
 	}
+if(){}
 ?>
